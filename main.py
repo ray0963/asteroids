039b87026a9abc9bff01a7cf34871981abe1390c
Ray Caldwell <rcaldwell963@gmail.com>
2024-09-05 22:35:10 +0100

    initial working commit

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
+
 from constants import *
+
+from player import Player
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill('black')
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
